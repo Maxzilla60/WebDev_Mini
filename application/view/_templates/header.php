958d06ae171eed5f1aae8a971ee27d554468acd8
Maxime Orione <maxpower98@hotmail.com>
2017-04-24 14:38:56 +0200

    Initial Commit

---
+++ application/view/_templates/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>WebDev: MINI PHP Framework</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- JS -->
+    <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
+    <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
+
+    <!-- CSS -->
+    <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
+</head>
+<body>
+    <!-- logo -->
+    <!--<div class="logo">
+        MINI
+    </div>-->
+    <img class="logo" style="height: 115px;" src="https://www.pxl.be/Assets/website/pxl_algemeen/afbeeldingen/grotere_versie/1314_logo_pxl_it.png"></img>
+
+    <!-- navigation -->
+    <div class="navigation">
+        <a href="<?php echo URL; ?>">home</a>
+        <!--<a href="<?php echo URL; ?>home/exampleone">subpage</a>
+        <a href="<?php echo URL; ?>home/exampletwo">subpage 2</a>
+        <a href="<?php echo URL; ?>songs">songs</a>-->
+        <a href="<?php echo URL; ?>webdev">webdev</a>
+    </div>
